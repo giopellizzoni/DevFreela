a7098e4b034578325fc5c46533b2a0e2cd0ba5bc
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-04-02 13:56:38 +0200

    Creating endpoints

--- DevFreela.API/Model/LoginModel.cs
+++ DevFreela.API/Model/LoginModel.cs
@@ -7,7 +7,7 @@ namespace DevFreela.API.Model
 {
     public class LoginModel
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
