b3c8af24eb72c8c6ed608cf1058721d463d8fa6c
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-04-05 17:15:44 +0200

    Refactoring Constructors on Controllers

--- DevFreela.API/Controllers/SkillsController.cs
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -4,20 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers;
 
-public class SkillsController : ControllerBase
+public class SkillsController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public SkillsController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet]
     public async Task<IActionResult> Get()
     {
         var query = new GetAllSkillsQuery();
-        var skills = await _mediator.Send(query);
+        var skills = await mediator.Send(query);
         return Ok(skills);
     }
     
--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -10,20 +10,13 @@ namespace DevFreela.API.Controllers;
 
 [Route("api/[controller]")]
 [Authorize]
-public class UsersController : ControllerBase
+public class UsersController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public UsersController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
         var getUserQuery = new GetUserQuery(id);
-        var userViewModel = await _mediator.Send(getUserQuery);
+        var userViewModel = await mediator.Send(getUserQuery);
         if (userViewModel == null) return NotFound();
         return Ok(userViewModel);
     }
@@ -40,7 +33,7 @@ public class UsersController : ControllerBase
                 .ToList();
             return BadRequest(messages);
         }
-        var id = await _mediator.Send(command);
+        var id = await mediator.Send(command);
         return CreatedAtAction(nameof(GetById), new { id = id }, command);
     }
 
@@ -48,7 +41,7 @@ public class UsersController : ControllerBase
     [AllowAnonymous]
     public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
     {
-        var loginUserViewModel = await _mediator.Send(command);
+        var loginUserViewModel = await mediator.Send(command);
         if (loginUserViewModel == null) return BadRequest();
         return Ok(loginUserViewModel);
     }
